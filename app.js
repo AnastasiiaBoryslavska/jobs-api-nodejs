de65a78a2706e1ed9a3744c054e092bd59dceb5d
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-10-22 17:59:06 -0400

    initial commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-// error handler
+// error handler  
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
