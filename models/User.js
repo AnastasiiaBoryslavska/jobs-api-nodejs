3f383e865859177174f6d9acbd4a5ace89af6625
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-10-24 22:25:11 -0400

    implemented JWT and password hashing

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -13,7 +15,7 @@ const UserSchema = new mongoose.Schema({
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     ],
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
@@ -23,4 +25,19 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-mongoose.exports = mongoose.model('User', UserSchema)
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+module.exports = mongoose.model("User", UserSchema);
