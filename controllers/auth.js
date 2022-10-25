ef9b2d80325ca85660bf150e69936f22e3df5dd1
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-10-24 22:13:25 -0400

    created token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,12 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors/bad-request");
+
 const register = async (req, res) => {
-  res.send("register user");
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+
+  res.status(StatusCodes.CREATED).json({ user: { user: user.name }, token });
 };
 
 const login = async (req, res) => {
